f70232403b7c88865211f09059fe7c92b0fc6f23
heeju <gmlwn1634@naver.com>
2020-11-16 19:15:44 +0900

    follow-me

--- app/src/main/java/com/example/followme_map/MainActivity.java
+++ app/src/main/java/com/example/followme_map/MainActivity.java
@@ -121,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    //gggee
 
     // cameraPosition 업데이트
     void setCameraPosition() {
